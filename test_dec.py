a49be0765d4cc0ae67125c51a0c8adeb9d7afc73
Jaison George <jaisongeorge699@gmail.com>
2026-02-24 09:56:00 +0530

    first commit

--- test_dec.py
+++ test_dec.py
@@ -1,5 +1,9 @@
 from app import storage
 
 # Retrieve decrypted
+<<<<<<< HEAD
 retrieved = storage.get_record("patient123")
+=======
+retrieved = storage.get_record("patient456")
+>>>>>>> 1139187 (Initial Commit)
 print("Retrieved record:", retrieved)
